72fb7837b7ca58100876692484b601e973616326
Shiva <shimar007@gmail.com>
2019-01-19 18:45:56 +1100

    Removing page title, main menu and logo initial setup

--- themes/developer/js/main.js
+++ themes/developer/js/main.js
@@ -59,19 +59,6 @@
 
 	});
 
-
-/*----------------------------------------------------*/
-/*	Make sure that #header-background-image height is
-/* equal to the browser height.
------------------------------------------------------- */
-
-   $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
-         $('header').css({ 'height': $(window).height() });
-         $('body').css({ 'width': $(window).width() })
-   });
-
-
 /*----------------------------------------------------*/
 /*	Fade In/Out Primary Navigation
 ------------------------------------------------------*/
