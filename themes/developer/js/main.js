14a4f11aecbcee5f3336dc1e12b1345b9f1d9e26
Shiva <shimar007@gmail.com>
2019-01-19 17:58:33 +1100

    Adding modules webform, twig field values

--- themes/developer/js/main.js
+++ themes/developer/js/main.js
@@ -67,9 +67,8 @@
 
    $('header').css({ 'height': $(window).height() });
    $(window).on('resize', function() {
-
-        $('header').css({ 'height': $(window).height() });
-        $('body').css({ 'width': $(window).width() })
+         $('header').css({ 'height': $(window).height() });
+         $('body').css({ 'width': $(window).width() })
    });
 
 
